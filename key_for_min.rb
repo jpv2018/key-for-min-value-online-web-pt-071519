0f168e8ebed84bbb42226cc650b7d7959ca62443
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 21:04:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,16 @@
 
 def key_for_min_value(name_hash)
   min_value = 1000000
-  min_value_key = nil
-  name_hash.collect do |key, value|
-    if value < min_value 
-      min_value = value
-      min_value_key = key
-      return min_value_key
+  min_value_key = "word"
+  if name_hash.length = 0
+    return nil
+  else    
+    name_hash.collect do |key, value|
+      if value < min_value 
+        min_value = value
+        min_value_key = key
+        return min_value_key
+      end
     end
   end
 end
